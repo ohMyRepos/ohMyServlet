5044d72dcd4d26631d20f2d09f51087133525317
zhanglintc <zhanglintc623@gmail.com>
2023-08-04 10:48:40 +0800

    plugins/install.bat

---
+++ plugins/install.bat
@@ -0,0 +1,5 @@
+@rem 将本插件安装到中央仓库，便于其他项目使用
+call mvn install:install-file -Dfile="./ohMyMojo.jar" -DgroupId="co.zhanglintc.plugins" -DartifactId="zhanglintc-ohMyMojo-plugin" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true" -DcreateChecksum="true"
+
+@echo press any key to close...
+@pause > nul
